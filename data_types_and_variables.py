5836c23308b6316f1dbe1024d0be580212a5cf0b
DeAdrien <deadrienhill@yahoo.com>
2023-02-07 16:02:21 -0600

    completed exercises for data types variables and operators

---
+++ data_types_and_variables.py
@@ -0,0 +1,58 @@
+##5. You have rented some movies for your kids:
+
+#The Little Mermaid for 3 days 
+#Brother Bear for 5 days
+#Hercules for 1 day
+#If the daily fee to rent a movie is 3 dollars, how much will you have to pay?
+ 
+9 * 3 
+
+#6. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook.
+
+#They pay you the following hourly rates:
+#Google: 400 dollars
+#Amazon: 380 dollars
+#Facebook: 350 dollars
+#This week you worked: 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+#How much will you receive in payment for this week?
+10 * 350 + 6 * 400 + 4 * 380 
+
+#7. A student can be enrolled to a class only if the class is not full 
+a#nd the class schedule does not conflict with her current schedule.
+
+enrolled_class = True
+class_not_full = False
+class_schedule_conflict = False
+
+class_not_full == class_schedule_conflict
+print(enrolled_class)
+
+#8. A product offer can be applied only if people buys more than 2 items, and the offer has not expired. 
+#Premium members do not need to buy a specific amount of products.
+
+regular_member_buy_two = True
+offer_expired = False
+premium_members_buy_two = False
+product_offer = True
+
+offer_expired != regular_member_buy_two 
+print(product_offer)
+
+premium_members_buy_two != regular_member_buy_two
+print(product_offer)
+
+9#. Continue working in the data_types_and_variables.py file. Use the following code to follow the instructions below:
+
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+password = 'jackrabbit'
+username = 'BuggsBunny'
+#Create a variable that holds a boolean value for each of the following conditions:
+
+#The password must be at least 5 characters
+#The username must be no more than 20 characters
+#The password must not be the same as the username
+#Bonus Neither the username or password can start or end with whitespace

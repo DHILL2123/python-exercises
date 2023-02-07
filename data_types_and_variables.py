7823987b98b29932daa61c7c9e1ed4be1f6ec15d
DeAdrien <deadrienhill@yahoo.com>
2023-02-07 16:58:18 -0600

    revised data type exercises

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -4,8 +4,12 @@
 #Brother Bear for 5 days
 #Hercules for 1 day
 #If the daily fee to rent a movie is 3 dollars, how much will you have to pay?
- 
-9 * 3 
+little_mermaid = 3
+brother_bear = 5
+hercules = 1
+daily_fee = 3
+little_mermaid * daily_fee + brother_bear * daily_fee + hercules * daily_fee
+27
 
 #6. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook.
 
@@ -14,12 +18,20 @@
 #Amazon: 380 dollars
 #Facebook: 350 dollars
 #This week you worked: 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+google_rate = 400
+amazon_rate = 380
+facebook_rate = 350
+fb_hrs = 10
+go_hrs = 6
+amaz_hrs = 4
+google_rate * go_hrs + amazon_rate * amaz_hrs + facebook_rate * fb_hrs
+7420
 
 #How much will you receive in payment for this week?
-10 * 350 + 6 * 400 + 4 * 380 
+# 10 * 350 + 6 * 400 + 4 * 380 = 7420
 
 #7. A student can be enrolled to a class only if the class is not full 
-a#nd the class schedule does not conflict with her current schedule.
+#and the class schedule does not conflict with her current schedule.
 
 enrolled_class = True
 class_not_full = False
@@ -34,7 +46,8 @@ print(enrolled_class)
 regular_member_buy_two = True
 offer_expired = False
 premium_members_buy_two = False
-product_offer = True
+product_offer = offer_expired != regular_member_buy_two and premium_members_buy_two != regular_member_buy_two
+print(product_offer)
 
 offer_expired != regular_member_buy_two 
 print(product_offer)
@@ -42,7 +55,9 @@ print(product_offer)
 premium_members_buy_two != regular_member_buy_two
 print(product_offer)
 
-9#. Continue working in the data_types_and_variables.py file. Use the following code to follow the instructions below:
+
+
+#9. Continue working in the data_types_and_variables.py file. Use the following code to follow the instructions below:
 
 
 username = 'codeup'
@@ -50,6 +65,13 @@ password = 'notastrongpassword'
 
 password = 'jackrabbit'
 username = 'BuggsBunny'
+
+min_password_length = len(password)>= 5
+max_username_length = len(username)<= 20
+
+password_username_unique = password != username
+valid_credentials = min_password_length and max_username_length and password_username_unique
+print(valid_credentials)
 #Create a variable that holds a boolean value for each of the following conditions:
 
 #The password must be at least 5 characters
